f0f0cb3bb55e4f1c8b308cadd6ade4819bdde745
heliaskaragozidis <!1Helias!!>
2022-02-26 00:07:14 +0200

    AuthGuard implemented

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {Router} from 'express';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    const jwtToken = localStorage.getItem('login_data');
+    if (jwtToken) {
+      return true;
+    } else {
+      this.router.navigate(['/pages/login']);
+      return false;
+    }
+  }
+  
+}
