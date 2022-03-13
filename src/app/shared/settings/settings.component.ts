d7458c152b03cb0e9677e9461ad4e7f80e4ced79
heliaskaragozidis <!1Helias!!>
2022-03-13 16:01:16 +0200

    Settings component updated

--- src/app/shared/settings/settings.component.ts
+++ src/app/shared/settings/settings.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
+import {AppConfig} from '../../dtos/app_config/app-config';
+
+declare var electron: any;
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, OnDestroy {
+  appConfigResponceEventHandler;
+  appConfig: AppConfig = new AppConfig();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _ngZone: NgZone) {
+    this.appConfigResponceEventHandler = (event, appConfig) => {
+      this._ngZone.run(() => {
+        this.appConfig = appConfig;
+        console.log(this.appConfig);
+      });
+    };
   }
 
+  ngOnInit(): void {
+    electron.ipcRenderer.on('app-config-response', this.appConfigResponceEventHandler);
+    electron.ipcRenderer.send('app-config-request', 'appConfig');
+  }
+
+  ngOnDestroy(): void {
+    electron.ipcRenderer.removeListener('app-config-response', this.appConfigResponceEventHandler);
+  }
+
+  save() {
+    console.log(this.appConfig);
+    electron.ipcRenderer.send('app-config-update', this.appConfig);
+  }
 }
