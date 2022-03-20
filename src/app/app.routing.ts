4d40512376eb9118e9ebb35ba6278f54ec47d227
heliaskaragozidis <!1Helias!!>
2022-03-20 15:07:56 +0200

    Angular upgraded to 12

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -26,7 +26,7 @@ export const AppRoutes: Routes = [
     children: [
         {
       path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+      loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(m => m.AdminLayoutModule)
   }]}
   // {
   //   path: '**',
