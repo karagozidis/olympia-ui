2f6cbb1e9a86035da406618e9e1dfa84a28aa1a2
heliaskaragozidis <!1Helias!!>
2022-03-20 15:56:52 +0200

    Package Upgrades

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import {Router} from 'express';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
